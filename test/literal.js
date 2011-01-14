67d0bf261409e1fd5a92fcd6d7a031fe2f311177
chrisdew <cmsdew+github@gmail.com>
2011-01-14 16:42:05 +0000

    broke it

--- test/literal.js
+++ test/literal.js
@@ -14,6 +14,17 @@ vows.describe('Literal Test').addBatch( {
 			assert.isFalse(topic.matched());
 			assert.isFalse(topic.failed());
 			assert.deepEqual(topic.completions(), ['hello']);
+		},
+		'parses "h"' : function(topic) {
+            topic.push("h");
+			assert.equal(topic.text, "hello");
+			assert.deepEqual(topic.expecting, ["o", "l", "l", "e"]);
+			assert.deepEqual(topic.matching, ["h"]);
+			assert.deepEqual(topic.failing, []);
+			assert.isTrue(topic.open());
+			assert.isFalse(topic.matched());
+			assert.isFalse(topic.failed());
+			assert.deepEqual(topic.completions(), ['ello']);
 		}
 	}
 }).export(module);
