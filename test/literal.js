444601c8b6faca3d1acc9e283abbb3f4a04f04e9
Chris Dew <cmsdew@gmail.com>
2011-01-16 12:32:26 +0000

    new tests

--- test/literal.js
+++ test/literal.js
@@ -49,4 +49,62 @@ vows.describe('Literal Test').addBatch( {
 			assert.deepEqual(topic.completions(), []);
 		}
 	}
+}).addBatch( {
+    'LITERAL("help")' : {
+        topic : p.LITERAL("help").spawn(),
+        'parses ""' : function(topic) {
+            assert.equal(topic.text, "help");
+            assert.deepEqual(topic.expecting, ["h", "e", "l", "p"]);
+            assert.deepEqual(topic.matching, []);
+            assert.deepEqual(topic.failing, []);
+            assert.isTrue(topic.open());
+            assert.isFalse(topic.matched());
+            assert.isFalse(topic.failed());
+            assert.deepEqual(topic.completions(), ['help']);
+        },
+        'parses "h"' : function(topic) {
+            topic.push("h");
+            assert.equal(topic.text, "help");
+            assert.deepEqual(topic.expecting, ["e", "l", "p"]);
+            assert.deepEqual(topic.matching, ["h"]);
+            assert.deepEqual(topic.failing, []);
+            assert.isTrue(topic.open());
+            assert.isFalse(topic.matched());
+            assert.isFalse(topic.failed());
+            assert.deepEqual(topic.completions(), ['elp']);
+        },
+        'parses "hel"' : function(topic) {
+            topic.push("e").push("l");
+            assert.equal(topic.text, "help");
+            assert.deepEqual(topic.expecting, ["p"]);
+            assert.deepEqual(topic.matching, ["h", "e", "l"]);
+            assert.deepEqual(topic.failing, []);
+            assert.isTrue(topic.open());
+            assert.isFalse(topic.matched());
+            assert.isFalse(topic.failed());
+            assert.deepEqual(topic.completions(), ['p']);
+        },
+        'parses "hell"' : function(topic) {
+            topic.push("l");
+            assert.equal(topic.text, "help");
+            assert.deepEqual(topic.expecting, ["p"]);
+            assert.deepEqual(topic.matching, ["h", "e", "l"]);
+            assert.deepEqual(topic.failing, ["l"]);
+            assert.isFalse(topic.open());
+            assert.isFalse(topic.matched());
+            assert.isTrue(topic.failed());
+            assert.deepEqual(topic.completions(), []);
+        },
+        'parses "hello"' : function(topic) {
+            topic.push("o");
+            assert.equal(topic.text, "help");
+            assert.deepEqual(topic.expecting, ["p"]);
+            assert.deepEqual(topic.matching, ["h", "e", "l"]);
+            assert.deepEqual(topic.failing, ["l", "o"]);
+            assert.isFalse(topic.open());
+            assert.isFalse(topic.matched());
+            assert.isTrue(topic.failed());
+            assert.deepEqual(topic.completions(), []);
+        }
+    }
 }).export(module);
